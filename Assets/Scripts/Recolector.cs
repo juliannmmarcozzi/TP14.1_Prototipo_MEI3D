d5571b8788928fb62c3e47773d2d4dee9ae9e4b5
JULIAN MARCOZZI <julianmarcozzi@gmail.com>
2026-05-27 14:18:03 -0300

    Clase 2 - 27/5

---
+++ Assets/Scripts/Recolector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recolector : MonoBehaviour
+{
+    int contador = 0;
+
+    void OnTriggerEnter(Collider objeto)
+    {
+        if(objeto.tag == "Coleccionable")
+        {
+            Destroy(objeto.gameObject);
+
+            contador = contador + 1;
+
+            Debug.Log(contador);
+        }
+    }
+}
